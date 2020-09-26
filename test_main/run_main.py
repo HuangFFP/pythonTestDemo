903debfb18c17a252bb333532c9b5eb90d10521a
huangfeipeng <270753136@qq.com>
2020-09-26 14:21:48 +0800

    commit

--- test_main/run_main.py
+++ test_main/run_main.py
@@ -3,10 +3,10 @@ import pytest
 import sys
 import os
 
-sys.path.append('../')
-sys.path.append('C:/Users/huangfeipeng/PycharmProjects/demo')
+# sys.path.append('../')
+# sys.path.append('C:/Users/huangfeipeng/PycharmProjects/demo')
 curPath = os.path.abspath(os.path.dirname(__file__))
-BasePath = curPath[:curPath.find("demo\\") + len("demo\\")]
+# BasePath = curPath[:curPath.find("demo\\") + len("demo\\")]
 
 if __name__ == "__main__":
     pytest.main(['-s', '-v', 'test_case/testRequest/', '-q', '--alluredir', 'reports'])
